bed17caf31c8ff1646408542292d6d1bd53c0aa5
Maxli <maxlicatenby@gmail.com>
2020-10-18 12:47:11 -0400

    Basic functionality

--- api_access.py
+++ api_access.py
@@ -1,8 +1,16 @@
 import fitbit
+import datetime
 
 keys = open("C:\\Keys\\fitbit.txt").read().split()
-id, secret, access_token, *_ = keys
 
-authd_client = fitbit.Fitbit(id, secret, access_token=access_token)
+## You should have a file at that path with ClientID, ClientSecret, access token, and refresh_token
+##all on a newline
 
-authd_client.get_sleep()
+id, secret, access_token, refresh_token, *_ = keys
+
+authd_client = fitbit.Fitbit(id, secret, access_token=access_token, refresh_token=refresh_token)
+
+sleep = authd_client.sleep()
+
+if __name__ == "__main__":
+    print(sleep)
